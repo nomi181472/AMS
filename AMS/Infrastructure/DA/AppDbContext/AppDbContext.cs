47efd6872719eca739ee41e68a59cefd9071b662
Noman Ali <33200799+nomi181472@users.noreply.github.com>
2024-07-30 18:01:41 +0500

    repo layer added

---
+++ AMS/Infrastructure/DA/AppDbContext/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DA.AppDbContexts
+{
+    public class AppDbContext:DbContext
+    {
+       
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+
+
+
+
+        }
+    }
+}
+
+
+
+
