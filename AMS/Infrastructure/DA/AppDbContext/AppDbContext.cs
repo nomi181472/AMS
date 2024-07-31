5117ed37c7306a06048afcc3c0144666dabdff4b
vizabdullah <k200131@nu.edu.pk>
2024-07-31 18:17:52 +0500

    Built relations in ModelBuilder.DbContext

--- AMS/Infrastructure/DA/AppDbContext/AppDbContext.cs
+++ AMS/Infrastructure/DA/AppDbContext/AppDbContext.cs
@@ -13,9 +13,15 @@ namespace DA.AppDbContexts
     {
         public DbSet<Allowance> Allowances { get; set; }
         public DbSet<Shift> Shifts { get; set; }
-
         public DbSet<Deduction> Deductions { get; set; }
         public DbSet<ShiftDeductionScheduler> ShiftDeductionSchedulers { get; set; }
+        public DbSet<AllowanceWorkingProfileManagement> AllowanceWorkingProfileManagements { get; set; }
+        public DbSet<Leave> Leaves { get; set; }   
+        public DbSet<LeaveWorkingProfileManagement> LeaveWorkingProfileManagements { get; set; }
+        public DbSet<WorkingProfile> WorkingProfiles { get; set; }
+
+        public DbSet<FiscalYear> FiscalYears { get; set; }
+
         public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
         {
 
@@ -23,6 +29,33 @@ namespace DA.AppDbContexts
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<AllowanceWorkingProfileManagement>()
+                .HasOne<Allowance>()
+                .WithMany()
+                .HasForeignKey(awm => awm.AllownaceId);
+
+            builder.Entity<AllowanceWorkingProfileManagement>()
+                .HasOne<WorkingProfile>()
+                .WithMany()
+                .HasForeignKey(awm => awm.WorkingProfileId);
+
+            builder.Entity<LeaveWorkingProfileManagement>()
+                .HasOne<Leave>()
+                .WithMany()
+                .HasForeignKey(lwm => lwm.LeaveId);
+
+            builder.Entity<LeaveWorkingProfileManagement>()
+                .HasOne<WorkingProfile>()
+                .WithMany()
+                .HasForeignKey(lwm => lwm.WorkingProfileId);
+
+            builder.Entity<WorkingProfile>()
+                .HasOne<FiscalYear>()
+                .WithMany()
+                .HasForeignKey(wp => wp.FiscalYearId);
+
+
 
         }
     }
