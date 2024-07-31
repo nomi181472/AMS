7a9357e017596380d488199121b4046b4fa6a4f5
AliAmaan0331 <aliamaan812@gmail.com>
2024-07-31 16:43:28 +0500

    Resolved Conflicts

---
+++ AMS/Infrastructure/DA/Models/Deduction.cs
@@ -0,0 +1,15 @@
+﻿using DA.Models.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DA.Models
+{
+    public class Deduction : Base<string>
+    {
+        public string Type { get; set; }
+        public string Description { get; set; }
+    }
+}
