9988b6f401fabffb224eec41a19ff40b05d5229a
AliAmaan0331 <aliamaan812@gmail.com>
2024-08-01 15:29:58 +0500

    working profile crud operatins

---
+++ AMS/Infrastructure/AttendanceServices/Services/WorkingProfileService/IWorkingProfileService.cs
@@ -0,0 +1,19 @@
+﻿using AttendanceServices.Services.AllowanceService.Models.Request;
+using AttendanceServices.Services.WorkingProfileService.Models.Request;
+using DA.Models.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceServices.Services.WorkingProfileService
+{
+    public interface IWorkingProfileService
+    {
+        Task<bool> AddWorkingProfile(CreateWorkingProfileRequest request, string userId, CancellationToken cancellationToken);
+        Task<bool> UpdateWorkingProfile(UpdateWorkingProfileRequest request, string UserId, CancellationToken cancellationToken);
+        Task<bool> DeleteWorkingProfileById(string Id, string UserId, CancellationToken cancellationToken);
+        Task<List<WorkingProfile>> ListAllWorkingProfiles(string UserId, CancellationToken cancellationToken);
+    }
+}
---
+++ AMS/Infrastructure/AttendanceServices/Services/WorkingProfileService/WorkingProfileService.cs
@@ -0,0 +1,34 @@
+﻿using AttendanceServices.Services.WorkingProfileService.Models.Request;
+using DA.Models.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceServices.Services.WorkingProfileService
+{
+    public class WorkingProfileService:IWorkingProfileService
+    {
+        public async Task<bool> AddWorkingProfile(CreateWorkingProfileRequest request, string userId, CancellationToken cancellationToken)
+        {
+            return true;
+        }
+
+        public async Task<bool> UpdateWorkingProfile(UpdateWorkingProfileRequest request, string userId, CancellationToken cancellationToken)
+        {
+            return true;
+        }
+
+        public async Task<bool> DeleteWorkingProfileById(string Id, string UserId, CancellationToken cancellationToken)
+        {
+            return true; 
+        }
+
+        public async Task<List<WorkingProfile>> ListAllWorkingProfiles(string UserId, CancellationToken cancellationToken)
+        {
+            List<WorkingProfile> workingProfiles = new List<WorkingProfile>();
+            return workingProfiles;
+        }
+    }
+}
