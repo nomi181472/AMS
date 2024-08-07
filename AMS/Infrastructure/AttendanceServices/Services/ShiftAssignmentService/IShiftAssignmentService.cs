1379b26f5986bb5c49697fafae734230387ad452
Usaid <pc@DESKTOP-PTRUD04>
2024-08-07 07:02:23 -0700

    -feature created interface for ShiftAssignmentService

---
+++ AMS/Infrastructure/AttendanceServices/Services/ShiftAssignmentService/IShiftAssignmentService.cs
@@ -0,0 +1,25 @@
+﻿using AttendanceServices.Services.ShiftAssignmentService.Request;
+using AttendanceServices.Services.ShiftAssignmentService.Response;
+using AttendanceServices.Services.ShiftManagementService.Models.Request;
+using AttendanceServices.Services.ShiftManagementService.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceServices.Services.ShiftAssignmentService
+{
+    public interface IShiftAssignmentService
+    {
+        Task<bool> AddShiftAssignment(RequestAddShiftWorkingProfile request, string userId, CancellationToken cancellationToken);
+
+        Task<List<ResponseGetShiftWorkingProfileWithDetails>> ListWithDetails(CancellationToken cancellationToken);
+
+        Task<ResponseGetShiftWorkingProfileWithDetails> SingleWithDetails(string shiftcode, CancellationToken cancellationToken);
+
+        Task<List<ResponseGetShiftWorkingProfileUpdated>> UpdateShiftAssignment(RequestUpdateShiftWorkingProfile request, string userId, CancellationToken cancellationToken);
+
+        Task<List<ResponseGetShiftWorkingProfileDeleted>> DeleteShiftAssignment(RequestDeleteShiftWorkingProfile request, string userId, CancellationToken cancellationToken);
+    }
+}
